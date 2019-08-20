98746fa40bcf3ffe4e5d7c302e04c1271ba2ca01
Wassim <root1@MacBook-Engneering-waseem.local>
2019-08-20 22:13:49 +0300

    New version

--- test.py
+++ test.py
@@ -3,6 +3,8 @@ with open(filepath) as fp:
    line = fp.readline()
    cnt = 1
    while line:
+
       # print("Line {}: {}".format(cnt, line.strip()))
+      
        line = fp.readline()
        cnt += 1
